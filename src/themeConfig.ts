c019f20a5d895ef0e4c9924270ed4359d9eeb5ec
Mohannad Adnan Alassar <mohannad.alassar@hotmail.com>
2024-04-16 20:37:00 +0300

    repush

--- src/themeConfig.ts
+++ src/themeConfig.ts
@@ -12,7 +12,7 @@ type themeConfigType = {
 
 export const themeConfig: themeConfigType = {
   name: 'L1',
-  API_URL: 'https://app-d89731df-3847-4a27-a904-ae31a58bb467.cleverapps.io/',
+  API_URL: 'https://app-d89731df-3847-4a27-a904-ae31a58bb467.cleverapps.io',
   localStorageName: 'it-trendco',
   PaletteOptions: {
     primary: {
